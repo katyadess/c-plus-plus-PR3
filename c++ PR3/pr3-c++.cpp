53a7c32eaae1152cd80f490ff6e336108bc5c1b2
Katya <Katya@DESKTOP-KDMV618>
2026-03-18 16:20:25 +0200

    створити структуру студента

---
+++ c++ PR3/pr3-c++.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+struct Student {
+	int number; // номер з.п
+	string lastName; // прізвище
+	string firstName; // ім'я
+	int birthYear; // рік народження
+	string Faculty; // факультет (МБФ, ІКФ, ЕФ, ЗФ)
+	string formOfEducation; // форма навчання (очна, заочна, вечірня)
+};
+
